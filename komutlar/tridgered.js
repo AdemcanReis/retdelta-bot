64354fbb8c5c944eb74c1dd61f92a80f4f18b277
AdemcanReis <43937213+AdemcanReis@users.noreply.github.com>
2018-10-09 08:40:06 +0300

    Create tridgered.js

---
+++ komutlar/tridgered.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+  message.channel.startTyping();
+  message.channel.send({
+    file: {
+      attachment: "https://cute-api.tk/v1/generate/triggered?url=" + message.author.avatarURL,
+      name: 'triggered.png'
+    }
+  });
+message.channel.stopTyping();
+};
+
+module.exports.help = {
+  name: 'triggered'
+};
