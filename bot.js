c0373ed91ab27ca15c28971d83456aab5d609b66
AdemcanReis <43937213+AdemcanReis@users.noreply.github.com>
2018-10-09 08:55:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,6 +102,33 @@ client.on("guildMemberAdd", member => {
 	
 });
 
+client.on("message", msg => {
+	const uyarıembed = new Discord.RichEmbed()
+      .setColor(0x00AE86)
+      .setDescription(":crown: " + msg.author + " Retdelta reklam koruması aktif, reklam yapmayı bırak evlat!:crown:")
+
+const dmembed = new Discord.RichEmbed()
+	.setTitle("Sunucunda " + msg.author.tag + " reklam yapıyor!")
+      .setColor(0x00AE86)
+      .setDescription("-warn <kişi> komutu ile onu uyarabilir ya da -kick <kişi> veya -ban <kişi> komutlarını kullanarak onu sunucudan uzaklaştırabilirsin!")
+	.addField("Kullanıcının mesajı:", "**" + msg.content + "**")
+
+if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)/g) && msg.channel.type === "text" && msg.channel.permissionsFor(msg.guild.member(client.user)).has("MANAGE_MESSAGES")) {
+	if(msg.member.hasPermission('BAN_MEMBERS')){
+	return;
+	} else {
+    msg.delete(30).then(deletedMsg => {
+     deletedMsg.channel.send(uyarıembed)
+	 msg.guild.owner.send(dmembed).catch(e => {
+            console.error(e);
+          });
+        }).catch(e => {
+          console.error(e);
+        });
+      };
+	  };
+    })
+
 ////////////////////////
 
 client.on('guildCreate', guild => {
