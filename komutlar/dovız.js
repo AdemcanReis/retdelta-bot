7bcc286a89a96be43faffe2f281a607d826d34a2
AdemcanReis <43937213+AdemcanReis@users.noreply.github.com>
2018-10-09 08:58:27 +0300

    Create dovız.js

---
+++ komutlar/dovız.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, message) => {
+var request = require('request');
+request('https://www.doviz.com/api/v1/currencies/USD/latest', function (error, response, body) {
+    if (error) return console.log('Hata:', error);
+    else if (!error) { 
+        var info = JSON.parse(body);
+request('https://www.doviz.com/api/v1/currencies/EUR/latest', function (error, response, body) {
+    if (error) return console.log('Hata:', error); 
+    else if (!error) { 
+        var euro = JSON.parse(body);
+      message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Dolar satış: ${info.selling} TL \nDolar alış: ${info.buying} TL \n\nEuro satış: ${euro.selling} TL \nEuro alış: ${euro.buying} TL`).setFooter('doviz.com alt yapısı kullanılarak hazırlanmıştır.').setColor("RED"));    }
+})
+    }
+})
+    };
+
+module.exports.help = {
+  name: 'döviz'
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['YEDEK KOMUT1', 'YEDEK KOMUT2'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "döviz",
+  description: "Şuanda  Döviz Nekadar ?",
+  usage: "döviz <mesaj>"
+};
